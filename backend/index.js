4eeabde510b039d8d589afff479c321fa4d8b479
rakesh <rakeshshinde5253@gmail.com>
2023-03-05 20:38:11 -1200

    multer config for upload file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { connection } = require('./Conectdb')
-
+const path = require('path');
+const multer = require('multer');
 var app = express();
+
+app.use('/assets', express.static(path.join(__dirname, '/public/assets')))
+
+
 dotenv.config();
 
 app.use(express.json())
 connection(process.env.Mongo_url);
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/assets');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single("file"), (req, res) => {
+    console.log(req.body);
+    res.status(200).json('file has been uploaded !')
+})
 
 var port = process.env.port || 3000;
 app.listen(port, () => {
