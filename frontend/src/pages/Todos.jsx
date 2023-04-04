10ab8f88422b52bb97e9b88838ce65d877c14aef
rakesh <rakeshshinde5253@gmail.com>
2023-04-04 19:43:14 +0530

    implement pagination

--- frontend/src/pages/Todos.jsx
+++ frontend/src/pages/Todos.jsx
@@ -10,16 +10,13 @@ import axios from 'axios'
 import TodoLoader from '../component/TodoLoader';
 import Error from '../component/Error';
 import CreateTaskmodal from '../component/CreateTaskmodal';
-
-
+import Pagination from '@mui/material/Pagination'
 const Wrapper = styled('div')(({ theme }) => ({
     display: 'flex',
     flexDirection: 'column',
     gap: '3rem',
-    height: '100vh',
+    Height: '70%',
     padding: '60px',
-
-
 }));
 
 
@@ -36,6 +33,24 @@ const Todos = () => {
     const [dayFilter, setdayFilter] = useState('last 7 days')
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false)
+    const [page, setPage] = useState(1);
+    const [data, setdata] = useState([]);
+    const itemsperpage = 6;
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+
+    function usePagination(data, itemsperpage) {
+        const totalpages = Math.ceil(data.length / itemsperpage)
+        const startindex = (page - 1) * itemsperpage;
+        const endindex = startindex + itemsperpage;
+        const filterdata = data.slice(startindex, endindex)
+        return { totalpages, filterdata }
+    }
+
+
+    const { totalpages, filterdata } = usePagination(data, itemsperpage)
+    console.log(totalpages, filterdata);
 
 
     useEffect(() => {
@@ -43,6 +58,7 @@ const Todos = () => {
             dispatch(todo_list_start());
             axios.get(`/task`).then((res) => {
                 dispatch(todo_list_success(res.data.tasks))
+                setdata(res.data.tasks)
             })
         } catch (error) {
             dispatch(todo_list_fail(error.response.data))
@@ -55,7 +71,7 @@ const Todos = () => {
 
         <Container maxWidth='xl' sx={{
             padding: '50px',
-            backgroundColor: '#edf2f4',
+            backgroundColor: '#ffffff',
 
         }} >
             <CssBaseline />
@@ -65,28 +81,36 @@ const Todos = () => {
                     margin: '0px 100px',
                     fontFamily: '"Poppins", sans serif !important',
                     fontWeight: '500',
-                    letterSpacing: '0.2rem'
+                    letterSpacing: '0.2rem',
+                    fontSize: '30px'
                 }} >
-                    Welcome back <span style={{
+                    Welcome back , <span sx={{
                         color: '#4c4645',
-                        fontWeight: '500',
-                        letterSpacing: '0.3rem'
+                        fontFamily: '"Dancing Script", cursive !important',
+                        letterSpacing: '0.3rem',
+                        fontWeight: 200
+
                     }} >{username}</span>
                 </Typography>
 
                 <Stack direction={'row'} spacing={5} sx={{
                     alignItems: 'center',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
+
                 }} >
                     <TextField placeholder='Search tasks ' size='small' sx={{
-
                         width: '300px'
                     }}></TextField>
-                    <Button variant='outlined' endIcon={<BsCalendarPlus />} onClick={handleOpen}>create</Button>
+                    <Button variant='contained' color='success' endIcon={<BsCalendarPlus />} onClick={handleOpen}>create</Button>
                     <CreateTaskmodal open={open} handleClose={handleClose} />
                 </Stack>
 
-                <Box sx={{ flexGrow: 1, padding: '30px 60px' }} >
+                <Box sx={{
+                    flexGrow: 1,
+                    padding: '30px 20px',
+                    minHeight: '70vh',
+
+                }} >
                     {error && <span style={{
                         marginTop: "50px",
                         textAlign: "center",
@@ -94,18 +118,30 @@ const Todos = () => {
                         justifyContent: 'center',
                         alignItems: 'center',
 
+
                     }}><Error severity="error" text={error} /></span>}
                     {loading && <TodoLoader />}
-                    <Grid container rowSpacing={5} columnSpacing={3} >
-                        {todos?.map((task, i) => (
+                    {filterdata.length > 0 ? <Grid container rowSpacing={5} columnSpacing={3} >
+                        {filterdata?.map((task, i) => (
                             <Grid item xs={12} md={4} lg={4} key={`${i}-${task.id}`} >
 
                                 <SingleTodo task={task} />
                             </Grid>
                         ))}
-                    </Grid>
+                    </Grid> : <Typography variant='h3' sx={{
+                        textAlign: 'center',
+                        margin: '100px auto',
+                        color: '#4a4e69'
+                    }}>No more todos..</Typography>}
                 </Box>
             </Wrapper>
+            <Pagination style={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'center',
+
+                padding: 10
+            }} count={10} page={page} onChange={handleChange} />
         </Container>
 
     );
